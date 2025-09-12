19bdc3fd38dfe7f35bc442a6a72316fadcaf9ef5
Mostafa Matar <engmostafamatar@gmail.com>
2025-09-12 21:56:01 +0200

    Fix app

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
+import { HelmetProvider } from "react-helmet-async";
 import Layout from "./components/Layout";
 import { LoadingSpinner } from "@/components/ui/loading";
 import { LazyErrorBoundary } from "@/components/ui/LazyErrorBoundary";
@@ -25,33 +26,35 @@ const SitemapPage = lazy(() => import("./pages/SitemapPage"));
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <LazyErrorBoundary>
-          <Suspense fallback={<LoadingSpinner />}>
-            <Routes>
-              <Route path="/" element={<Layout><Index /></Layout>} />
-              <Route path="/browse" element={<Layout><BrowsePage /></Layout>} />
-              <Route path="/gem/:id" element={<Layout><GemDetailPage /></Layout>} />
-              <Route path="/login" element={<Layout><LoginPage /></Layout>} />
-              <Route path="/register" element={<Layout><RegisterPage /></Layout>} />
-              <Route path="/create" element={<Layout><CreateGemPage /></Layout>} />
-              <Route path="/email-confirmation" element={<Layout><EmailConfirmation /></Layout>} />
-              <Route path="/complete-profile" element={<Layout><CompleteProfile /></Layout>} />
-              <Route path="/edit-profile" element={<Layout><EditProfile /></Layout>} />
-              <Route path="/terms" element={<Layout><TermsOfServicePage /></Layout>} />
-              <Route path="/privacy" element={<Layout><PrivacyPolicyPage /></Layout>} />
-              <Route path="/sitemap" element={<Layout><SitemapPage /></Layout>} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Suspense>
-        </LazyErrorBoundary>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <LazyErrorBoundary>
+            <Suspense fallback={<LoadingSpinner />}>
+              <Routes>
+                <Route path="/" element={<Layout><Index /></Layout>} />
+                <Route path="/browse" element={<Layout><BrowsePage /></Layout>} />
+                <Route path="/gem/:id" element={<Layout><GemDetailPage /></Layout>} />
+                <Route path="/login" element={<Layout><LoginPage /></Layout>} />
+                <Route path="/register" element={<Layout><RegisterPage /></Layout>} />
+                <Route path="/create" element={<Layout><CreateGemPage /></Layout>} />
+                <Route path="/email-confirmation" element={<Layout><EmailConfirmation /></Layout>} />
+                <Route path="/complete-profile" element={<Layout><CompleteProfile /></Layout>} />
+                <Route path="/edit-profile" element={<Layout><EditProfile /></Layout>} />
+                <Route path="/terms" element={<Layout><TermsOfServicePage /></Layout>} />
+                <Route path="/privacy" element={<Layout><PrivacyPolicyPage /></Layout>} />
+                <Route path="/sitemap" element={<Layout><SitemapPage /></Layout>} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </Suspense>
+          </LazyErrorBoundary>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </HelmetProvider>
 );
 
 export default App;
